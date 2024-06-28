e4e3678cbeb578d6445a161752c86f33f0d36124
Camellia.xiaohua <camellia.xiaohua@qq.com>
2024-06-28 14:25:27 +0800

    linux

---
+++ servlet05/src/servlet/PostServlet.java
@@ -0,0 +1,23 @@
+package servlet;
+
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @Datetime: 2024/6/28下午1:53
+ * @author: Camellia.xioahua
+ */
+public class PostServlet extends GenericServlet {
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        // 响应一些内容到浏览器上。
+        res.setContentType("text/html;charset=utf-8");
+        PrintWriter out = res.getWriter();
+        out.println("post servlet");
+    }
+}
