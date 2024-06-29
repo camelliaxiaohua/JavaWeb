d40d56554e34a02b641638665b6f21e3d1ee7641
Camellia.xiaohua <2421195723@qq.com>
2024-06-29 09:15:53 +0800

    代码

---
+++ servlet07/src/servlet/WelcomeServlet.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @Datetime: 2024/6/29上午8:42
+ * @author: Camellia.xioahua
+ */
+public class WelcomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>Welcome to Servlet</h1>");
+    }
+}
